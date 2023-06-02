65180ce67c5d31ef0d60f23ef172246ad8931050
RakhimM <132366835+RakhimM@users.noreply.github.com>
2023-06-02 09:16:26 +0600

    Update Mazhitov.c
    Добавлена возможность вычитания чисел

--- Mazhitov.c
+++ Mazhitov.c
@@ -4,6 +4,8 @@ int main(){
     int a = 5;
     int b = 3;
     int sum = a + b;
+    int defference = a - b;
     printf("Summa: %d\n", sum);
+    printf("Defference: %d\n",difference")
     return 0;
 }
